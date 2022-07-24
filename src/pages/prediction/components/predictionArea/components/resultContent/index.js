5b1d29ea37c3197352d99922fa306140c0420d40
zhangchen <244551528@qq.com>
2022-07-24 17:10:33 +0800

    fix: 修复了识别结果不展示的bug

--- src/pages/prediction/components/predictionArea/components/resultContent/index.js
+++ src/pages/prediction/components/predictionArea/components/resultContent/index.js
@@ -4,10 +4,11 @@ import { Typography } from '@alifd/next';
 const { H1, H2, Paragraph } = Typography;
 
 export const ResultContent = memo((props) => {
-  const { chineseName, description, methon} = props;
+  const { chineseName, description, methon} = props?.bugData;
+  console.log(props)
 
   return (
-    <div className="resultContent" style={{width: '100%'}}>
+    <div style={{width: '100%'}}>
       <Typography >
         <H1>识别结果</H1>
         {
--- src/pages/prediction/components/predictionArea/index.js
+++ src/pages/prediction/components/predictionArea/index.js
@@ -44,42 +44,46 @@ export const PredictionArea = memo(() => {
 
   return (
     <div className="predictionArea">
-      <div className="steps">
-        <Step current={step}>
-        <Step.Item title="上传图片" />
-        <Step.Item title="智能识别结果" />
-        </Step>
-      </div>
-      <div className="content">
-        <div className="upLoad">
-            <img src={photo1} alt="bugsPhoto"/>
-            <Upload
-              action={`${Serve}:${port}/postPhoto`}
-              onSuccess={upLoadPhotoSuccess}
-              withCredentials={false}
-              name="image"
-            >
-              <Button 
-              type="secondary"
-              onClick={upLoadPhoto.onclick}
-              key={upLoadPhoto.action}
+      <div className='predictionContent'>
+        <h2>上传图片进行识别</h2>
+        <div className="steps">
+          <Step current={step}>
+          <Step.Item title="上传图片" />
+          <Step.Item title="智能识别结果" />
+          </Step>
+        </div>
+        <div className="content">
+          <div className="upLoad">
+              <img src={photo1} alt="bugsPhoto"/>
+              <Upload
+                action={`${Serve}:${port}/postPhoto`}
+                onSuccess={upLoadPhotoSuccess}
+                withCredentials={false}
+                name="image"
               >
-                {upLoadPhoto.action}
-              </Button>      
-            </Upload>
-        </div>
-          <div className="resultPhoto">
-            <img src={photo2} alt="resultPhoto" /> 
-            <Button
-              type="secondary" 
-              onClick={prediction}
-              loading={resultGetting}
-            >
-              点击进行识别
-            </Button>
+                <Button 
+                type="secondary"
+                onClick={upLoadPhoto.onclick}
+                key={upLoadPhoto.action}
+                >
+                  {upLoadPhoto.action}
+                </Button>      
+              </Upload>
           </div>
-        </div>
-        <div className='resultContent'>
+            <div className="resultPhoto">
+              <img src={photo2} alt="resultPhoto" /> 
+              <Button
+                type="secondary" 
+                onClick={prediction}
+                loading={resultGetting}
+              >
+                点击进行识别
+              </Button>
+            </div>
+          </div>
+        
+      </div>
+      <div className='resultContent'>
           {
             photo2.search(`${Serve}`) !== -1
             ? <ResultContent bugData={bugData}/>
